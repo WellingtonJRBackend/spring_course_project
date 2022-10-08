5ca2c6ac9a90bd86dbe9c6e02f0d97e0c4c59400
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-10-08 10:42:21 -0300

    map entities

--- src/main/java/com/spring_course_project/domain/Request.java
+++ src/main/java/com/spring_course_project/domain/Request.java
@@ -1,9 +1,11 @@
 package com.spring_course_project.domain;
 
+import com.spring_course_project.enums.RequestState;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,14 +13,32 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "request")
 public class Request {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name = "creation_date",nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date creationDate;
 	
+	@Column(length = 12,nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id",nullable = false)
 	private User user;
+	
+	@OneToMany(mappedBy = "request")
 	List<RequestStage> stages = new ArrayList<>();
 }
--- src/main/java/com/spring_course_project/domain/User.java
+++ src/main/java/com/spring_course_project/domain/User.java
@@ -5,21 +5,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "user")
 public class User {
 	
-		private Long id;
-		private String name;
-		private String email;
-		private String password;
-		private Role role;
-		
-		private List<Request> requests = new ArrayList<>();
-		private List<RequestStage> stages = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 75, nullable = false)
+	private String name;
+	
+	@Column(length = 75,nullable = false,unique = true)
+	private String email;
+	
+	@Column(length = 100,nullable = false)
+	private String password;
+	
+	@Column(length = 20,nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Request> requests = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "user")
+	private List<RequestStage> stages = new ArrayList<>();
 	}
 
